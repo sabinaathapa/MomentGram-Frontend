e53a758865467db6d3eac09b0e0fe8f914c53cb8
unknown <Sabina Thapa@DESKTOP-DEV.greentick.com.np>
2024-06-14 16:29:25 +0545

    Post upload added.

--- src/Route.tsx
+++ src/Route.tsx
@@ -1,12 +1,16 @@
 import React from "react";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import HomePage from "./components/HomePage";
+import SocialFeed from "./components/Feed/SocialMediaFeed";
+import CreatePostForm from "./components/Feed/UploadFeed";
 
 const RouterApp: React.FC = ()=>{
     return(
         <Router>
             <Routes>
                 <Route path="/" element={<HomePage/>} />
+                <Route path="/feed" element={<SocialFeed/>} />
+                <Route path="/post" element={<CreatePostForm/>}/>
             </Routes>
         </Router>
     )
